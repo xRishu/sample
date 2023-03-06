f3dfafb1762304dd47773299b5356702401efffa
Rishu Kumar <99220430+xRishu@users.noreply.github.com>
2023-03-07 00:18:56 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+        # Code to send email goes here
+        return 'Message sent!'
+    else:
+        return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
